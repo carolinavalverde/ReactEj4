824c93d288d163971817af51950c7b71d310f980
Caro Valverde <carolina.valverde255@gmail.com>
2024-01-12 16:04:52 -0300

    agrego codigo base

---
+++ src/components/Titulo.jsx
@@ -0,0 +1,10 @@
+const Titulo = ({ comision, anioActualProps }) => {
+  return (
+    <>
+      <h2>Titulo generado con props de la comision: {comision}</h2>
+      <p>Año actual: {anioActualProps}</p>
+    </>
+  );
+};
+
+export default Titulo;
