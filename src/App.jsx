9a8a261e86f4f29852b3c65bbbf3a7166f997fd8
Caro Valverde <carolina.valverde255@gmail.com>
2024-01-17 20:17:39 -0300

    agrego css

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import { useState } from "react";
-import TaskList from "./components/TaskList";
-import TaskInput from "./components/TaskInput";
+import React from "react";
+import "./styles/App.css";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  const addTask = (newTask) => {
-    setTasks([...tasks, newTask]);
-  };
-
-  return (
-    <div className="App">
-      <h1>Lista de Tareas</h1>
-      <TaskInput addTask={addTask} />
-      <TaskList tasks={tasks} />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
