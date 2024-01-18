321f6ec656ed1344ab63a40cdcf4512012608d06
Caro Valverde <carolina.valverde255@gmail.com>
2024-01-17 21:50:04 -0300

    cambio css a sass e instalo sass

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
 
-const Checkbox = () => {
-	return <div>Checkbox!</div>;
+const Checkbox = props => {
+  // (A)
+  const {
+    onChange,
+    data: { id, description, done }
+  } = props;
+  return (
+    <Fragment>
+      <label className="todo new-item">
+        {/*(B) (C)*/}
+        <input
+          className="todo__state"
+          name={id}
+          type="checkbox"
+          defaultChecked={done}
+          onChange={onChange}
+        />
+        <div className="todo__text">{description}</div>
+      </label>
+    </Fragment>
+  );
 };
 
 export default Checkbox;
