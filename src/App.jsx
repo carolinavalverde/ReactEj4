4fbfb8946fc384da937b33112ca34ce4afc28f86
Caro Valverde <carolina.valverde255@gmail.com>
2024-01-30 13:55:47 -0300

    agrego estilos

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import TodoApp from "./components/ToDoApp.jsx";
 
 function App() {
   return (
-    <section className="container m-3 d-flex">
-      <div className="card shadow rounded col-8 justify-content-center pb-5 pe-3">
-        <TodoApp />
+    <section className="container m-3 d-flex justify-content-center">
+      <div className="col-8 ">
+        <div className="card shadow rounded pb-5 pe-3">
+          <TodoApp />
+        </div>
       </div>
     </section>
   );
