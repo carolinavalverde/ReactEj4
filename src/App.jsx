f354837f1eb84217ee4c6768b209e0884d08229a
Caro Valverde <carolina.valverde255@gmail.com>
2024-01-30 11:58:24 -0300

    agrego estilos

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import Container from "./components/Container";
 function App() {
   return (
     <div className="App">
-      <h1 className="card shadow text-center text-bg-primary m-2 p-2">
-        Ej4 y Ej5: To do List
+      <h1 className="card shadow text-center bg-warning text-bg-dark fw-bold m-2 p-2">
+        Ej4 y Ej5: To Do List
       </h1>
       <Container />
     </div>
